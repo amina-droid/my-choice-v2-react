8060f595aca6ce567ae0c3981547da8e84025c2d
emazurov <emazurov@ozon.ru>
2021-03-25 00:53:54 +0300

    : return : return close

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -37,7 +37,7 @@ const Login = () => {
       }
 
       // eslint-disable-next-line react/no-this-in-sfc
-      // this.close();
+      this.close();
       window.removeEventListener('message', authHandler);
 
       try {
