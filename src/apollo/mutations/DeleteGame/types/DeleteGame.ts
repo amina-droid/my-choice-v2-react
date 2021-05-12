87ae37a1909bfd87a91a65576f95b7ec418be903
Amina <amina.mazurova42@gmail.com>
2021-05-12 14:59:59 +0300

    files

---
+++ src/apollo/mutations/DeleteGame/types/DeleteGame.ts
@@ -0,0 +1,16 @@
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL mutation operation: DeleteGame
+// ====================================================
+
+export interface DeleteGame {
+  deleteGame: boolean;
+}
+
+export interface DeleteGameVariables {
+  gameId: any;
+}
