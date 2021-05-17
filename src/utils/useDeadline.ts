d6c9b64201237715490d32a881c2536a7455c5d2
emazurov <emazurov@ozon.ru>
2021-05-17 13:38:56 +0300

    : fix : fix eslint useDeadline

--- src/utils/useDeadline.ts
+++ src/utils/useDeadline.ts
@@ -1,11 +1,16 @@
 import moment, { MomentInput } from 'moment';
-import { isNil } from 'lodash';
 import { useMemo } from 'react';
 
 const useDeadline = (time: MomentInput): string | undefined => {
-  return useMemo(() => isNil(time) ? undefined : moment(time)
-    .add(window.timeDiff, 'ms')
-    .toISOString(), [time])
+  return useMemo(() => {
+    if (!time) {
+      return undefined;
+    }
+
+    return moment(time)
+      .add(window.timeDiff, 'ms')
+      .toISOString();
+  }, [time]);
 };
 
 export default useDeadline;
