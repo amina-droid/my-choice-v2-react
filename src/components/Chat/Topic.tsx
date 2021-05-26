db0f4298525f50b559d91314882d09a8c5243e66
emazurov <emazurov@ozon.ru>
2021-05-26 16:14:04 +0300

    : add : add messages load more

--- src/components/Chat/Topic.tsx
+++ src/components/Chat/Topic.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useMemo, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { useLazyQuery, useMutation } from '@apollo/client';
 
 import Comment from 'antd/es/comment';
