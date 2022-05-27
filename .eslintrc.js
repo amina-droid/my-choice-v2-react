9cfa06cad58c47c3d1fceb341e8fefb398a1a809
deivoff <deivoff@gmail.com>
2022-05-27 18:56:15 +0500

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -38,7 +38,8 @@ module.exports = {
   },
   plugins: ['react', 'graphql', 'react-hooks', '@typescript-eslint'],
   rules: {
-    'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
+    'security/detect-object-injection': 0,
+    'react-hooks/rules-of-hooks': 'warn', // Checks rules of Hooks
     'react-hooks/exhaustive-deps': 'warn', // Checks effect dependencies
     'react/jsx-one-expression-per-line': 0,
     'react/jsx-props-no-spreading': [
